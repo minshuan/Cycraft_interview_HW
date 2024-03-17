7dec292314d94cc3e026f40e557d18ab7b3545ab
minshuan <610821221@gms.ndhu.edu.tw>
2024-03-17 15:25:08 +0800

    Summary

--- tinydb.py
+++ tinydb.py
@@ -21,9 +21,11 @@ class TinyVectorDB:
             self.data = pd.read_json('data.json')
             print("load data")
             self.data['EmbVector'] = self.data['EmbVector'].apply(np.array)
-            self.load_index()
+            self.load_hnsw_index()
         else:
             self.data = pd.DataFrame(columns=['Document', 'EmbVector'])
+            self.hnsw_index = None
+            self.lsh_index = None
 
     # use model embedding->768
     def insert(self, sequence):
@@ -31,24 +33,24 @@ class TinyVectorDB:
         # print(vector.shape)
         self.data = self.data._append({'Document': sequence, 'EmbVector': vector}, ignore_index=True)
     
-    def build_index(self):
-        self.index = nmslib.init(method='hnsw', space='cosinesimil')
+    def build_hnsw_index(self):
+        self.hnsw_index = nmslib.init(method='hnsw', space='cosinesimil')
         self.index.addDataPointBatch(self.data['EmbVector'].tolist())
         self.index.createIndex({'post': 2}, print_progress=True)
-        self.save_index()
-    def save_index(self):
-        self.index.saveIndex('index.bin')
-        print("index save")
-    def load_index(self):
-        if os.path.exists('index.bin'):
-            self.index = nmslib.init(method='hnsw', space='cosinesimil')
-            self.index.loadIndex('index.bin')
-            print("load index")
+        self.save_hnsw_index()
+    def save_hnsw_index(self):
+        self.index.saveIndex('hnsw_index.bin')
+        print("hnsw_index save")
+    def load_hnsw_index(self):
+        if os.path.exists('hnsw_index.bin'):
+            self.hnsw_index = nmslib.init(method='hnsw', space='cosinesimil')
+            self.hnsw_index.loadIndex('index.bin')
+            print("load hnsw_index")
         else:
-            self.build_index()
+            self.build_hnsw_index()
 
     def search_similar_hnsw(self, query_vector, n):
-        idxs, _ = self.index.knnQuery(query_vector, 2*n)
+        idxs, _ = self.hnsw_index.knnQuery(query_vector, 2*n)
         similarities = []
         for i in idxs:
             sim = cosine_sim(query_vector, self.data['EmbVector'].iloc[i])
@@ -64,7 +66,7 @@ class TinyVectorDB:
             similarities.append((sim, self.data.iloc[i]['Document']))
         similarities.sort(key=lambda x: x[0], reverse=True)
         top_n = [[(sim, doc) for sim, doc in similarities[:n]]]
-        return top_n
+        return top_n    
     
     def save(self):
         self.data.to_json('data.json', orient='records')
